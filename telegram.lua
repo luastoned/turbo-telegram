c31f054e11b160c45897397f9bdf0e5069262e15
LuaStoned <gluastoned@gmail.com>
2016-08-25 14:28:03 +0200

    Updated to Bot API 2.1

--- telegram.lua
+++ telegram.lua
@@ -1,8 +1,8 @@
 local telegram = {
-	_VERSION		= "v1.1.0",
-	_DESCRIPTION	= "Telegram bot for Turbo.lua",
+	_VERSION		= "v1.2.0",
+	_DESCRIPTION	= "Telegram Bot API for Turbo.lua",
 	_URL			= "https://github.com/luastoned/turbo-telegram",
-	_LICENSE		= [[Copyright (c) 2015 @LuaStoned]],
+	_LICENSE		= [[Copyright (c) 2016 @LuaStoned]],
 	
 	botToken		= nil,
 }
@@ -63,27 +63,11 @@ local function request(path, request, multi)
 	return json.result
 end
 
-local function getMe()
-	return request("getMe")
-end
+-- Getting updates
 
 local function getUpdates(options)
 	options = options or {}
 	return request("getUpdates", options)
-end
-
-local function getUserProfilePhotos(user_id, options)
-	requireArg(user_id, "number")
-	
-	options = options or {}
-	options.user_id = user_id
-	
-	return request("getUserProfilePhotos", options)
-end
-
-local function getFile(file_id)
-	requireArg(file_id, "number")
-	return request("getUpdates", {file_id = file_id})
 end
 
 local function setWebhook(url)
@@ -91,8 +75,14 @@ local function setWebhook(url)
 	return request("setWebhook", {url = url})
 end
 
+-- Available methods
+
+local function getMe()
+	return request("getMe")
+end
+
 local function sendMessage(chat_id, text, options)
-	requireArg(chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(text, "string")
 
 	options = options or {}
@@ -103,15 +93,15 @@ local function sendMessage(chat_id, text, options)
 end
 
 local function forwardMessage(chat_id, from_chat_id, message_id)
-	requireArg(chat_id, "number")
-	requireArg(from_chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	requireArg(from_chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(message_id, "number")
 	
 	return request("forwardMessage", {chat_id = chat_id, from_chat_id = from_chat_id, message_id = message_id})
 end
 
 local function sendPhoto(chat_id, options)
-	requireArg(chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(options, "table")
 	
 	local multipart = false
@@ -130,7 +120,7 @@ local function sendPhoto(chat_id, options)
 end
 
 local function sendAudio(chat_id, options)
-	requireArg(chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(options, "table")
 	
 	local multipart = false
@@ -149,7 +139,7 @@ local function sendAudio(chat_id, options)
 end
 
 local function sendDocument(chat_id, options)
-	requireArg(chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(options, "table")
 	
 	local multipart = false
@@ -168,7 +158,7 @@ local function sendDocument(chat_id, options)
 end
 
 local function sendSticker(chat_id, options)
-	requireArg(chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(options, "table")
 	
 	local multipart = false
@@ -187,7 +177,7 @@ local function sendSticker(chat_id, options)
 end
 
 local function sendVideo(chat_id, options)
-	requireArg(chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(options, "table")
 	
 	local multipart = false
@@ -205,8 +195,27 @@ local function sendVideo(chat_id, options)
 	return request("sendVideo", options, multipart)
 end
 
+local function sendVoice(chat_id, options)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	requireArg(options, "table")
+	
+	local multipart = false
+	options.chat_id = chat_id
+	if (options.file_id) then
+		options.voice = options.file_id
+	else
+		multipart = true
+		options.voice = {
+			filename = options.filename or "moon.ogg",
+			data = options.voice,
+		}
+	end
+	
+	return request("sendVoice", options, multipart)
+end
+
 local function sendLocation(chat_id, latitude, longitude, options)
-	requireArg(chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(latitude, "number")
 	requireArg(longitude, "number")
 	
@@ -218,8 +227,38 @@ local function sendLocation(chat_id, latitude, longitude, options)
 	return request("sendLocation", options)
 end
 
+local function sendVenue(chat_id, latitude, longitude, title, address, options)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	requireArg(latitude, "number")
+	requireArg(longitude, "number")
+	requireArg(title, "string")
+	requireArg(address, "string")
+	
+	options = options or {}
+	options.chat_id = chat_id
+	options.latitude = latitude
+	options.longitude = longitude
+	options.title = title
+	options.address = address
+	
+	return request("sendVenue", options)
+end
+
+local function sendContact(chat_id, phone_number, first_name, options)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	requireArg(phone_number, "string")
+	requireArg(first_name, "string")
+	
+	options = options or {}
+	options.chat_id = chat_id
+	options.phone_number = phone_number
+	options.first_name = first_name
+	
+	return request("sendContact", options)
+end
+
 local function sendChatAction(chat_id, action)
-	requireArg(chat_id, "number")
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
 	requireArg(action, "string")
 	
 	-- typing for text messages
@@ -231,13 +270,130 @@ local function sendChatAction(chat_id, action)
 	return request("sendChatAction", {chat_id = chat_id, action = action})
 end
 
+local function getUserProfilePhotos(user_id, options)
+	requireArg(user_id, "number") -- TODO: chat_id can be number or string
+	
+	options = options or {}
+	options.user_id = user_id
+	
+	return request("getUserProfilePhotos", options)
+end
+
+-- https://api.telegram.org/file/bot<token>/<file_path>
+local function getFile(file_id)
+	requireArg(file_id, "number")
+	return request("getFile", {file_id = file_id})
+end
+
+local function kickChatMember(chat_id, user_id)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	requireArg(user_id, "number")
+	
+	return request("kickChatMember", {chat_id = chat_id, user_id = user_id})
+end
+
+local function leaveChat(chat_id)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	
+	return request("leaveChat", {chat_id = chat_id})
+end
+
+local function unbanChatMember(chat_id, user_id)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	requireArg(user_id, "number")
+	
+	return request("unbanChatMember", {chat_id = chat_id, user_id = user_id})
+end
+
+local function getChat(chat_id)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	
+	return request("getChat", {chat_id = chat_id})
+end
+
+local function getChatAdministrators(chat_id)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	
+	return request("getChatAdministrators", {chat_id = chat_id})
+end
+
+local function getChatMembersCount(chat_id)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	
+	return request("getChatMembersCount", {chat_id = chat_id})
+end
+
+local function getChatMember(chat_id, user_id)
+	requireArg(chat_id, "number") -- TODO: chat_id can be number or string
+	requireArg(user_id, "number")
+	
+	return request("getChatMember", {chat_id = chat_id, user_id = user_id})
+end
+
+local function answerCallbackQuery(callback_query_id 	, options)
+	requireArg(callback_query_id, "string")
+	
+	options = options or {}
+	options.callback_query_id = callback_query_id
+	
+	return request("answerCallbackQuery", options)
+end
+
+-- Updating messages
+
+local function editMessageText(text, options)
+	-- (chat_id and message_id) or inline_message_id
+	requireArg(text, "string")
+
+	options = options or {}
+	options.text = text
+	
+	return request("editMessageText", options)
+end
+
+local function editMessageCaption(caption, options)
+	-- (chat_id and message_id) or inline_message_id
+	requireArg(caption, "string")
+
+	options = options or {}
+	options.caption = caption
+	
+	return request("editMessageCaption", options)
+end
+
+local function editMessageReplyMarkup(reply_markup, options)
+	-- (chat_id and message_id) or inline_message_id
+	requireArg(reply_markup, "string")
+
+	options = options or {}
+	options.reply_markup = reply_markup
+	
+	return request("editMessageReplyMarkup", options)
+end
+
+-- Inline mode
+
+local function answerInlineQuery(inline_query_id, results, options)
+	requireArg(inline_query_id, "string")
+	requireArg(results, "table")
+
+	options = options or {}
+	options.inline_query_id = inline_query_id
+	options.results = results
+	
+	return request("answerInlineQuery", options)
+end
+
+
+-- Internal
 telegram.setBotToken = setBotToken
 
-telegram.getMe = getMe
+-- Getting updates
 telegram.getUpdates = getUpdates
-telegram.getUserProfilePhotos = getUserProfilePhotos
-telegram.getFile = getFile
 telegram.setWebhook = setWebhook
+
+-- Available methods
+telegram.getMe = getMe
 telegram.sendMessage = sendMessage
 telegram.forwardMessage = forwardMessage
 telegram.sendPhoto = sendPhoto
@@ -245,8 +401,21 @@ telegram.sendAudio = sendAudio
 telegram.sendDocument = sendDocument
 telegram.sendSticker = sendSticker
 telegram.sendVideo = sendVideo
+telegram.sendVoice = sendVoice
 telegram.sendLocation = sendLocation
+telegram.sendVenue = sendVenue
+telegram.sendContact = sendContact
 telegram.sendChatAction = sendChatAction
+telegram.getUserProfilePhotos = getUserProfilePhotos
+telegram.getFile = getFile
+
+-- Updating messages
+telegram.editMessageText = editMessageText
+telegram.editMessageCaption = editMessageCaption
+telegram.editMessageReplyMarkup = editMessageReplyMarkup
+
+-- Inline mode
+telegram.answerInlineQuery = answerInlineQuery
 
 -- allow raw debug calls
 local meta = {
